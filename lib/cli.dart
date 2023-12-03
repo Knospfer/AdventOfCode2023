f7c19b521f170f6d5de69589e43add8ea6626e87
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-12-03 12:35:22 +0100

    feat: day 1 2

--- lib/cli.dart
+++ lib/cli.dart
@@ -1,4 +1,7 @@
 import 'dart:io';
+import 'dart:math';
+
+import 'package:collection/collection.dart';
 
 void day_1_1() async {
   final content = await File('assets/day_1_1.txt').readAsString();
@@ -26,26 +29,11 @@ void day_1_2() async {
 
   final foundNumbers = <int>[];
 
-  final stringToNumbers = {
-    "eight": 8,
-    "nine": 9,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7
-  };
-
   for (var line in content.split('\n')) {
-    for (final entry in stringToNumbers.entries) {
-      line = line.replaceAll(entry.key, entry.value.toString());
-    }
-    print(line);
-
     final numbers = <int>[];
-    for (final c in line.split('')) {
+    final updatedLine = _replaceLiteralNumbers(line);
+
+    for (final c in updatedLine.split('')) {
       final parsed = int.tryParse(c);
       if (parsed != null) numbers.add(parsed);
     }
@@ -57,4 +45,42 @@ void day_1_2() async {
   }
 
   print(foundNumbers.reduce((value, element) => value + element));
+}
+
+String _replaceLiteralNumbers(String line) {
+  final numberMap = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+  };
+
+  String result = "";
+
+  for (int i = 0; i < line.length; i++) {
+    final digit = line[i];
+    if (digit.contains(RegExp('1|2|3|4|5|6|7|8|9'))) {
+      result += digit;
+      continue;
+    }
+
+    if (!digit.contains(RegExp('o|t|f|s|e|n'))) continue;
+
+    for (final MapEntry(:key, :value) in numberMap.entries) {
+      final endSection = min(i + key.length, line.length);
+
+      final section = line.substring(i, endSection);
+
+      if (section != key) continue;
+
+      result += value.toString();
+    }
+  }
+
+  return result;
 }
